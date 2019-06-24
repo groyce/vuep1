4d599d6980feec4c8b2faaa810d13b1652f42643
groyce <groyce@unomaha.edu>
2019-06-24 15:01:57 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';  // Add this
+import router from './router'
+Vue.use(VeeValidate);  // Add this
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,               // Add this line
+  render: h => h(App)
 }).$mount('#app')
